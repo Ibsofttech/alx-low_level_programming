a430e25797688e44540f944e278aebc2cb536bdf
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-04-27 23:56:17 +0100

    link list first

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a structure list_t list
+ * @h: a pointer to access the elemet of the structure
+ * Return: node numbers in the list
+ */
+size_t print_list(const list_t *h)
+{
+        size_t j;
+
+        for (j = 0; h; j++)
+        {
+                printf("[%d] %s\n", h->len, h->str);
+                h = h->next;
+        }
+        return (j);
+}
