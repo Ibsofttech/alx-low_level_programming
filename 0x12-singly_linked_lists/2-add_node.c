6653d6f064a40bbca10eba41d2a210d8869a080c
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-04-27 16:48:58 -0700

    corrextion

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
 /**
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
  * add_node - addition of new nodes to the list
  * @head: current node
  * @str: string to add to the head
@@ -8,20 +22,28 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	int i, len;
+	char *content;
 	list_t *new;
-	unsigned int len = 0;
 
-	while (str[len])
-		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
+	if (str == NULL || head == NULL)
 		return (NULL);
-
-	new->str = strdup(str);
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
 	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
-	return (*head);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
