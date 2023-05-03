a0e667a5e457d350199c78b96dd5ff67a439eb81
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-05-03 13:51:44 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the element of a link list
+ * @h: list
+ *
+ * Return: number of nodes of a link list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		j++;
+	}
+	return (i);
+}
