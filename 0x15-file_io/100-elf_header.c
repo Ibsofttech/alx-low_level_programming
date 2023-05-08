751fb55e5996de68a743498e62bf473820c4ae7e
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-05-08 09:26:00 -0700

    display

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -A function that copies the content of a file to another file.
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: 0 on succes, -1 on error.
+ */
+
+int main(int argc, char *argv[])
+{
+return (1);
+return (0);
+}
