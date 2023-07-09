e0f949012e1d88992e9a74a2d52a87e53aa2061d
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-07-08 18:38:55 -0700

    endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * get_endianness -A function that Checks the endianness.
+ *
+ * Return: 0 if big endians.
+ *         1 if little endians.
+ */
+int get_endianness(void)
+{
+	int number = 1;
+	char *endians = (char *)&number;
+
+	if (*endians == 1)
+		return (1);
+
+	return (0);
+}
