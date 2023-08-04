bd980fb1d8e465310df30ba881acbc804b3984be
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-08-04 01:17:53 +0000

    endianess in a number

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * get_endianness -A function that Checks the endianness.
+ * get_endianness -A function that Checks the endianness in a number.
  *
  * Return: 0 if big endians.
  *         1 if little endians.
  */
 int get_endianness(void)
 {
-	int number = 1;
-	char *endians = (char *)&number;
+	int numb = 1;
+	char *endians_num = (char *)&numb;
 
-	if (*endians == 1)
+	if (*endians_num == 1)
 		return (1);
 
 	return (0);
