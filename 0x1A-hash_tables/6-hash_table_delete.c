2dcaf8d2912ead0e7832cfde6d0fed8f11076c76
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-10-11 17:38:48 +0000

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,24 +8,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int index;
-	hash_node_t *temp1;
-	hash_node_t *temp2;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int idx;
 
-	if (ht == NULL)
-		return;
-
-	for (index = 0; index < ht->size; index++)
+	for (idx = 0; idx < ht->size; idx++)
 	{
-		temp1 = ht->array[index];
-		while ((temp2 = temp1) != NULL)
+		if (ht->array[idx] != NULL)
 		{
-			temp1 = temp1->next;
-			free(temp2->key);
-			free(temp2->value);
-			free(temp2);
+			node = ht->array[idx];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
